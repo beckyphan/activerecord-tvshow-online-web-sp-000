e895b9b35bd80a09a33d7cb827aac00e1ec4b946
Becky Phan <beckyphan@live.com>
2019-08-02 22:35:08 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,7 @@ class CreateShows << ActiveRecord::Migration[5.2]
   
   def change 
     create_table :shows |t|
-      t.string :name 
+      t.string :name
       t.string :network 
       t.string :day 
       t.integer :rating
